1531ec455d2e79df3cffe05506e8e310ff3b5b18
N1ghting4le <mikita.salavei@modsen-software.com>
2026-05-08 15:23:30 +0300

    refactor: add useAdminGuard hook for AdminLoginPage

---
+++ src/pages/AdminLoginPage/hooks/useAdminGuard.ts
@@ -0,0 +1,15 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
+
+import { ADMIN_PRODUCTS_ROUTE } from "@/constants/routes";
+import { isAuthorizedAsAdmin } from "@/utils/isAuthorizedAsAdmin";
+
+export const useAdminGuard = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isAuthorizedAsAdmin()) {
+      navigate(ADMIN_PRODUCTS_ROUTE);
+    }
+  }, []);
+};
--- src/pages/AdminLoginPage/index.tsx
+++ src/pages/AdminLoginPage/index.tsx
@@ -1,17 +1,12 @@
-import { Navigate } from "react-router";
-
 import { Header } from "@/components/Header";
-import { ADMIN_PRODUCTS_ROUTE } from "@/constants/routes";
 import { AdminLoginForm } from "@/forms/AdminLoginForm";
-import { isAuthorizedAsAdmin } from "@/utils/isAuthorizedAsAdmin";
 
 import { LOGIN_TEXT } from "./constants";
+import { useAdminGuard } from "./hooks/useAdminGuard";
 import classes from "./styles.module.css";
 
 export function AdminLoginPage() {
-  if (isAuthorizedAsAdmin()) {
-    return <Navigate to={ADMIN_PRODUCTS_ROUTE} replace />;
-  }
+  useAdminGuard();
 
   return (
     <>
